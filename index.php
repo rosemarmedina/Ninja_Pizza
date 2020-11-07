a6ac6ccaf0266a1d90ad4d97b5a3075698f63353
Rosemar <medinarosemar@gmail.com>
2020-11-07 18:24:08 +0900

    Ninja Pizza Index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+    <?php include('header.php'); ?>
+    <?php include('footer.php'); ?>
+
+</html>
