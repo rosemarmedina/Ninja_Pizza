9249e3c55e0157de85b77e0fa437a18fc9768060
Rosemar <medinarosemar@gmail.com>
2020-11-13 00:20:45 +0900

    The Explode Function

--- index.php
+++ index.php
@@ -42,7 +42,11 @@ mysqli_close($conn);
                 <div class="card z-depth-0">
                     <div class="card-content center">
                         <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-                        <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                        <ul>
+                            <?php foreach(explode(',', $pizza['ingredients']) as $ing){ ?>
+                            <li><?php echo htmlspecialchars($ing); ?></li>
+                            <?php } ?>
+                        </ul>
                     </div>
                     <div class="card-action right align">
                         <a href="#" class="brand-text">more info</a>
