3d1e2dd570b484b7fd254281b1df0221b97ad159
Rosemar <medinarosemar@gmail.com>
2020-11-12 23:23:07 +0900

    Rendering Data to the Browser

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if(!$conn){
 }
 
 //write a query for all pizza
-$sql = 'SELECT title, ingredients, id FROM pizzas';
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at'; //we added order by to arrange the array according to when its created
 
 //make a query and get result
 $result = mysqli_query($conn, $sql);
@@ -20,7 +20,8 @@ mysqli_free_result($result);
 
 //closing connection
 mysqli_close($conn);
-print_r($pizzas);
+
+//deleted the "print_r($pizzas);" because we dont need it anymore. We just run it for test if our query $pizzas worked 
 
 
 ?>
@@ -32,6 +33,26 @@ print_r($pizzas);
 </head>
 
     <?php include('header.php'); ?>
+
+    <h4 class="center grey-text">Pizzas!</h4>
+    <div class="container">
+    <div class="row">
+        <?php foreach($pizzas as $pizza){ ?>
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                        <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                    </div>
+                    <div class="card-action right align">
+                        <a href="#" class="brand-text">more info</a>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+    </div>
+
     <?php include('footer.php'); ?>
 
 </html>
